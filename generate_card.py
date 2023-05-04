c104371b1e57ba468860dbd87c3bc64eff0a8db8
Jan Dohring <jan.dohring@zemind.ca>
2023-05-04 10:29:11 -0400

    realigned decks

--- generate_card.py
+++ generate_card.py
@@ -65,13 +65,23 @@ def generate_back():
 
 def generate_deck(name : str, cards : list[Card]):
     files = [f'{CARDS_DIR}/{card.Name}.png' for card in cards]
-    files.append(f'{CARDS_DIR}/back.png')
 
-    new_image = Image.new('RGBA', size=(size.x * len(files), size.y))
+    Y_GRID_SIZE = 2
+    X_GRID_SIZE = (len(cards) + 1) // 2
+
+    new_image = Image.new('RGBA', size=(size.x * X_GRID_SIZE, size.y * Y_GRID_SIZE))
 
     for i, file in enumerate(files):
+        x_coord = i // 2
+        y_coord = i % 2
+
         image = Image.open(file)
-        new_image.paste(image, (size.x * i, 0, size.x * (i + 1), size.y))
+        new_image.paste(image, (size.x * x_coord, size.y * y_coord, size.x * (x_coord + 1), size.y * (y_coord + 1))) 
+        # Because minimum exports are 2 high, we double the height
+
+    x_coord = X_GRID_SIZE - 1
+    y_coord = 1
+    new_image.paste(Image.open(f'{CARDS_DIR}/back.png'), (size.x * x_coord, size.y * y_coord, size.x * (x_coord + 1), size.y * (y_coord + 1)))
     
     new_image.save(f'{DECKS_DIR}/{name}.png')
 
