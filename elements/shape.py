7d83d049ff42f8314f228929685fb54263deb938
Jan D <jandohring@hotmail.com>
2023-07-16 17:43:52 -0400

    color verification

--- elements/shape.py
+++ elements/shape.py
@@ -2,7 +2,7 @@ from PIL import Image
 from core.alignment import Alignment
 from elements.element import CardElement
 from core.geometry import Point, Rect
-from core.color import Color
+from core.color import Color, make_color
 from core.schema import Schema
 from PIL import ImageDraw
 from abc import abstractmethod
@@ -10,9 +10,9 @@ from abc import abstractmethod
 class ShapeElement(CardElement):
     def __init__(self, fill : Color, offset: Point, size: Point, alignment: Alignment = None, outline : Color = None, outlineWidth : int = 0, children : list[CardElement] = None) -> None:
         super().__init__(offset, alignment, size, children)
-        self.fill = fill
-        self.outline = outline
-        self.outlineWidth = outlineWidth
+        self.fill = make_color(fill)
+        self.outline = make_color(outline)
+        self.outlineWidth = make_color(outlineWidth)
     
     def draw(self, image: Image, entry: dict[str, str], schema: Schema, parent_area: Rect, index : int = 0):
         area : Rect = self.calculate_size(parent_area)
