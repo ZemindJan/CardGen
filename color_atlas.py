378046d20bd19348c1aed7091f240dc7e3f7e79c
Jan D <jandohring@hotmail.com>
2023-07-16 17:28:51 -0400

    starting atlas

--- color_atlas.py
+++ color_atlas.py
@@ -1,7 +1,5 @@
 colors = {
     'white' : '#ffffff',
     'black' : '#000000',
-    'red'   : '#ff0000',
-    'green' : '#00ff00',
     'blue'  : '#0000ff',
 }
---
+++ location_cards.py
@@ -0,0 +1,2 @@
+from core.schema import Schema
+
