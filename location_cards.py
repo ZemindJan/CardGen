9e46cb2544277257a7e28d5d70b244fa4b2e5207
Jan D <jandohring@hotmail.com>
2023-07-16 17:37:42 -0400

    schema works

--- location_cards.py
+++ location_cards.py
@@ -2,15 +2,23 @@ from core.schema import Schema
 from data.source import OnlineSource
 from elements.text import TextElement
 from elements.rect import RectElement
+from core.alignment import TopCenter
+from core.geometry import Point
 
 src = OnlineSource('https://docs.google.com/spreadsheets/d/e/2PACX-1vQd8e0poc7VR1-vKW3GnrVuywCL0IHOHAMfUpW3m90ctsUOgClQL04NuzVUNid8Q5Cb9PwjGT5hXPdt/pub?gid=0&single=true&output=csv')
 
 schema = Schema(
-    name='locations/$name$',
+    naming='locations/$name$',
     elements=[
         TextElement(
             text='$name$', 
             font_path='squealer', 
-            fill='blue')
+            fill='blue',
+            font_size=40,
+            offset=Point(0, 50),
+            alignment=TopCenter
+        )
     ]
 )
+
+schema.process(src)
