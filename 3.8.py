5f51db526ab85d8c6fedc7d9378535edacf552a7
Jan D <jandohring@hotmail.com>
2023-07-17 14:14:18 -0400

    created 3.8

---
+++ 3.8.py
@@ -0,0 +1,10 @@
+from cardsmith import *
+
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQfcuVJtXMn4PGIY7id1qMlOi4IbkVqFpqSArPBe-YZlKZ2crQuPx_IzHGe5O8z86uPB93QAykHNT4T/pub?gid=802293359&single=true&output=csv"
+
+schema = Schema(
+    name='$name$',
+    elements=[
+        
+    ]
+)
