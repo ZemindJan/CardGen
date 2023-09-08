e68e4f7d8006aebb6ea359a8423dea6c7e9cb0f9
Jan D <jandohring@hotmail.com>
2023-09-08 18:49:33 -0400

    Update safe_paste.py

--- core/safe_paste.py
+++ core/safe_paste.py
@@ -5,7 +5,10 @@ def safe_paste(canvas : Image, image : Image, area : Rect, transparency_mask : I
     area = area.rounded()
     
     adjusted_p1 = Point(max(0, area.p1.x), max(0, area.p1.y))
-    adjusted_p2 = Point(min(canvas.size[0], area.p2.x), min(canvas.size[1], area.p2.y))
+    adjusted_p2 = Point(
+        min(canvas.size[0], area.p2.x), 
+        min(canvas.size[1], area.p2.y)
+    )
     adjusted_area = Rect(adjusted_p1, adjusted_p2)
 
     if adjusted_p1 != area.p1 or adjusted_p2 != area.p2:
