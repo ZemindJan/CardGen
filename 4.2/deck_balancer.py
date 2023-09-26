8531aa9fe16b91255c64f449732de7b88ff0ea83
Jan Dohring <jan.dohring@zemind.ca>
2023-09-26 11:01:55 -0400

    Update deck_balancer.py

--- 4.2/deck_balancer.py
+++ 4.2/deck_balancer.py
@@ -22,6 +22,8 @@ src = OnlineSource(url)
 deck_property = 'deck'
 suit_property = 'suit'
 count_property = 'copies'
+acost_property = 'acost'
+dcost_property = 'dcost'
 
 @dataclass
 class Deck:
@@ -82,11 +84,11 @@ for entry in src.get_data():
 for deck in decks.values():
     print(f'-----')
     print(f'{deck.name.upper()}')
-    print(f'High Cards:   {deck.high_cards}')
-    print(f'Mid Cards:    {deck.mid_cards}')
-    print(f'Low Cards:    {deck.low_cards}')
-    print(f'Skill Cards:  {deck.skill_cards}')
-    print(f'Total Cards:  {deck.total}')
+    print(f'High Cards:   {deck.high_cards}'.ljust(18) + f'Attack Costs'.ljust(12) + f'Defense Costs')
+    print(f'Mid Cards:    {deck.mid_cards}'.ljust(18) + f'0 Cost: '.ljust(12) + f'0 Cost: ')
+    print(f'Low Cards:    {deck.low_cards}'.ljust(18) + f'1 Cost: '.ljust(12) + f'1 Cost: ')
+    print(f'Skill Cards:  {deck.skill_cards}'.ljust(18) + f'2 Cost: ')
+    print(f'Total Cards:  {deck.total}'.ljust(18) + f'3+ Cost: ')
     
     print(f'HeavyWeight:  {"✅" if deck.could_be_heavyweight() else "❌"}')
     print(f'MiddleWeight: {"✅" if deck.could_be_middleweight() else "❌"}')
